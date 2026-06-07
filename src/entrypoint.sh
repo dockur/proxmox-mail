f7ca0e6b79db4de6e999d0de753cbbf2ce720b31
Kroese <kroese@users.noreply.github.com>
2026-06-07 16:06:40 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -84,7 +84,7 @@ chown "$user:$user" "$dir" || :
 
 dir="/etc/proxmox-datacenter-manager/auth"
 mkdir -p "$dir"
-chown "root:$user" "$dir" || :
+chown "root:root" "$dir" || :
 
 dir="/var/lib/proxmox-datacenter-manager"
 mkdir -p "$dir"
@@ -130,8 +130,8 @@ cleanup() {
   fi
 
   info "Waiting for services to stop.."
-  wait "$API_PID" || :
-  wait "$PRIV_API_PID" || :
+  wait "${API_PID:-}" || :
+  wait "${PRIV_API_PID:-}" || :
 
   echo "Shutdown completed succesfully."
   exit 0
